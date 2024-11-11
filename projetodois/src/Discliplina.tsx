62e0659e9b3c5e99bc7cbfacb4b814f8777382d8
TheDevCaio <caioinnocencio@gmail.com>
2024-11-11 20:29:44 -0300

    Salvando mudanças

---
+++ projetodois/src/Discliplina.tsx
@@ -0,0 +1,17 @@
+
+function  Disciplina(){
+return (
+
+<div>
+<main>
+<section>
+<h1>Disciplina - Frontend</h1>
+<p>Eu faço programas</p>
+</section>
+</main>
+</div>
+);
+
+}
+
+export default Disciplina;
